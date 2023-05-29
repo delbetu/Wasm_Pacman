3b8e3f93a85cdf4f6ccd770f807776904f5158d0
M. Bellucci <delbetu@gmail.com>
2023-05-29 17:21:45 -0300

    keep rectangle inside canvas limits

--- ruby/game.rb
+++ ruby/game.rb
@@ -2,6 +2,8 @@ require 'js'
 
 FPS = 60.freeze
 canvas = JS.global[:document].querySelector('#myCanvas')
+@max_width = 400 - 50
+@max_height = 400 - 50
 @ctx = canvas.getContext("2d")
 TIME_PER_FRAME = (1000 / FPS).freeze
 @x = 0
@@ -14,13 +16,13 @@ def draw_rectangle
   @ctx.fillRect(@x, @y, 50, 50)
 end
 
-def move_rectangle_left = @x -= @speedX
-def move_rectangle_right = @x += @speedX
-def move_rectangle_top = @y -= @speedY
-def move_rectangle_bottom = @y += @speedY
+def move_rectangle_left = @x = [ @x - @speedX, 0 ].max
+def move_rectangle_right = @x = [ @x + @speedX, @max_width ].min
+def move_rectangle_top = @y = [ @y - @speedY, 0].max
+def move_rectangle_bottom = @y = [ @y + @speedY, @max_height].min
 
 def draw
-  @ctx.clearRect(0,0, 400, 400)
+  @ctx.clearRect(0,0, @max_width, @max_height)
   draw_rectangle
   move_rectangle_bottom
 end
