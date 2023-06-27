ee97ddf670a46ff621d0235c48aafcaebd2452fa
M. Bellucci <delbetu@gmail.com>
2023-06-27 11:00:31 -0300

    Make hero to look to the direction it is moving.
    Each sprite row represents the hero moving in one direction.
    Sprite created with https://www.piskelapp.com/p/create/sprite

--- ruby/game/hero.rb
+++ ruby/game/hero.rb
@@ -15,12 +15,16 @@ module Moveable
   def move(direction)
     case direction
     when :left
+      set_sprite_row(:left) # TODO: coupled to includer
       @x -= @speed_x if within_limits?(@x - @speed_x, @y)
     when :right
+      set_sprite_row(:right)
       @x += @speed_x if within_limits?(@x + @speed_x, @y)
     when :up
+      set_sprite_row(:up)
       @y -= @speed_y if within_limits?(@x, @y - @speed_y)
     when :down
+      set_sprite_row(:down)
       @y += @speed_y if within_limits?(@x, @y + @speed_y)
     end
   end
@@ -37,29 +41,54 @@ end
 
 class Hero
   include Moveable
-  def initialize(x,y, canvas, context)
+  HEIGHT = 32.freeze
+  WIDTH = 32.freeze
+  SPEED_X = 2
+  SPEED_Y = 2
+  SPRITE = "./assets/sprite.png".freeze
+
+  def initialize(x, y, canvas, context)
     @canvas = canvas
     @context = context
-    @current_frame = 1
-    height = width = 16
-    super(x, y, 2, 2, width, height, canvas)
+    @frame_col = 0
+    @frame_row = 2
+    @frame_sequence = circular(from: 0, to: 2)
+    super(x, y, SPEED_X, SPEED_Y, WIDTH, HEIGHT, canvas)
   end
 
-  def draw_sprite
-    # open/close mouth
-    every(0.2) do
-      @current_frame = ( @current_frame + 1 ) % 3
+  def increment_sprite_col
+    every(0.2) do # open/close mouth
+      @frame_col = @frame_sequence.()
+    end
+  end
+
+  def set_sprite_row(direction)
+    case direction
+    when :left
+      @frame_row = 0
+    when :right
+      @frame_row = 1
+    when :up
+      @frame_row = 2
+    when :down
+      @frame_row = 3
     end
   end
 
   def draw
-    draw_sprite
+    increment_sprite_col
     image = JS.eval("return new Image()")
-    image[:src] = "./assets/sprite.png"
+    image[:src] = SPRITE
     @context.drawImage(
       image,
-      @current_frame * width,
-      0, width, height, @x, @y, width, height
+      ( @frame_col * width ),# start x
+      ( @frame_row * height ), # start y
+      width, # width x
+      height, # height y
+      @x,
+      @y,
+      width,
+      height
     )
   end
 
@@ -74,5 +103,10 @@ class Hero
       yield
     end
   end
-end
 
+  # circular(from: 4, to: 9) ==> loops through 4 to 9 each time it is called
+  def circular(from: 0, to:)
+      frame_col = from - 1
+      ->() { from + (frame_col += 1) % (to - from + 1) }
+  end
+end
